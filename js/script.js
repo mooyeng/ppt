84666b1778bbe6c617e79e1e28d7e79050d36a89
mooyeng <104557999+mooyeng@users.noreply.github.com>
2022-05-14 19:21:26 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 10) {
+        $(".navbar").addClass("nav-scroll");
+        $(".back_to_top").addClass("show_btn");
+    } else {
+        $(".navbar").removeClass("nav-scroll");
+        $(".back_to_top").removeClass("show_btn");
+    }
+});
+
+//banshawali//
+
+$(function () {
+    $('.genealogy-tree ul').hide();
+    $('.genealogy-tree>ul').show();
+    $('.genealogy-tree ul.active').show();
+    $('.genealogy-tree li').on('click', function (e) {
+        var children = $(this).find('> ul');
+        if (children.is(":visible")) children.hide('fast').removeClass('active');
+        else children.show('fast').addClass('active');
+        e.stopPropagation();
+    });
+});
