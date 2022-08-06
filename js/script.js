fdd68988a9b80aae48a745643f4b6885d68a7d1f
mooyeng <i.mooyang@gmail.com>
2022-08-06 06:24:09 +0545

    quiz

--- js/script.js
+++ js/script.js
@@ -10,19 +10,7 @@ $(window).scroll(function() {
     }
 });
 
-//banshawali//
 
-$(function () {
-    $('.genealogy-tree ul').hide();
-    $('.genealogy-tree>ul').show();
-    $('.genealogy-tree ul.active').show();
-    $('.genealogy-tree li').on('click', function (e) {
-        var children = $(this).find('> ul');
-        if (children.is(":visible")) children.hide('fast').removeClass('active');
-        else children.show('fast').addClass('active');
-        e.stopPropagation();
-    });
-});
 
 //electronics//
 MathJax.Hub.Config({
@@ -30,5 +18,6 @@ MathJax.Hub.Config({
     });
 
 
-//wordmeaning//
+
+
 
